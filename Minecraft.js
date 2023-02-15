a7ec8f5ce4b7b67202313599538d9fb3ef7472b9
AnLaVN <binhan29122003@gmail.com>
2023-02-15 23:22:43 +0700

    ASM Final

--- Minecraft.js
+++ Minecraft.js
@@ -7,6 +7,15 @@ var app = angular.module("MyApp", ["ngRoute"]);
 		$http.get('DLC_Detail.json').then(
 			function(response){
 				$scope.ListDLC = response.data;
+				$scope.ListItem = angular.copy($scope.ListDLC)
+				$scope.ListItemOther = []
+				$http.get('DLC_Other.json').then(
+					function(response){
+						$scope.ListItemOther = response.data;
+						for(i in $scope.ListItemOther){
+							$scope.ListItem.push($scope.ListItemOther[i])
+						}
+				});
 		});
 
 		$scope.AddCart = function(p){
@@ -20,13 +29,36 @@ var app = angular.module("MyApp", ["ngRoute"]);
 			const toast = new bootstrap.Toast(document.getElementById('Toast'))
 			toast.show()
 		}
+
+		$scope.LIndex = 0
+		$scope.LList=[1,2,3,4,5,6]
+		$scope.page = function(index){
+			index=index-1
+			$scope.LIndex = (index < 0 ? 0 : index)*2
+		}
+		$scope.next = function(){
+			$scope.LIndex = $scope.LIndex >= 0 && $scope.LIndex < $scope.ListItem.length - 2 ? $scope.LIndex + 2 : $scope.LIndex
+		}
+		$scope.back = function(){
+			$scope.LIndex = $scope.LIndex >= 2 && $scope.LIndex < $scope.ListItem.length ? $scope.LIndex - 2 : 0
+		}
+
+		$rootScope.Search = ""
+		
+		$rootScope.Page = "Cart"
+		$scope.$watch("$resolve", function(value) {
+			$rootScope.Page = value.$resolve;
+	   });
 	});	
+
 	app.controller("DetailCtrl", function ($scope, $routeParams){
 		$scope.index = $routeParams.index
 	});
-	app.controller("DLCCtrl", function ($scope, $http){
-		$scope.ListDLC = $scope.ListDLC 
+
+	app.controller("DLCCtrl", function ($scope){
+		// $scope.ListDLC = $scope.ListDLC 
 	});
+
 	app.controller("NewDungeonsCtrl", function ($scope){
 		$scope.ListNew = [
 			{Image: "https://www.minecraft.net/content/dam/games/dungeons/logos/vinyl_1x1.jpg", Name: "MINECRAFT TRÊN VINYL!", Text:"Những ngọn đồi sống động với âm thanh lo-fi!"},
@@ -35,6 +67,7 @@ var app = angular.module("MyApp", ["ngRoute"]);
 			{Image: "https://www.minecraft.net/content/dam/games/dungeons/key-art/spookyfall22-1x1.jpg", Name: "MÙA THU TUYỆT VỜI", Text: "Bóng đêm đang vẫy gọi bạn, liệu bạn sẽ bỏ cuộc..."}
 		]
 	});
+
 	app.controller("NewLegendsCtrl", function ($scope){
 		$scope.ListNew = [
 			{Image: "https://www.minecraft.net/content/dam/games/badger/game-characters/Legends_newfriends_tile.jpg", Name: "KẺ THÙ CŨ TRỞ THÀNH BẠN MỚI", Text:"Những đồng minh quen thuộc nhưng không ngờ tới trong Legends"},
@@ -43,17 +76,7 @@ var app = angular.module("MyApp", ["ngRoute"]);
 			{Image: "https://www.minecraft.net/content/dam/games/badger/game-characters/Legends_MobdDD_tile.jpg", Name: "NHẬT KÝ PHÁT TRIỂN ĐÃ RA MẮT", Text: "Chúng tôi nói về những người bạn quen, kẻ thù cũ, và mũ đội đầu."}
 		]
 	});
-	app.controller("MarketplaceCtrl", function ($scope, $http) {
-		$scope.ListItem = angular.copy($scope.ListDLC)
-		$scope.ListItemOther = []
-		$http.get('DLC_Other.json').then(
-			function(response){
-				$scope.ListItemOther = response.data;
-				for(i in $scope.ListItemOther){
-					$scope.ListItem.push($scope.ListItemOther[i])
-				}
-		});
-	});
+
 	app.controller("CartCtrl", function ($scope, $rootScope){
 		if($rootScope.Carts === undefined) $rootScope.Carts = []
 		$scope.getTotal = function(){
@@ -69,21 +92,27 @@ var app = angular.module("MyApp", ["ngRoute"]);
 		}
 	});
 	
-
-
 	app.config(function ($routeProvider) {
 		$routeProvider
 			.when("/Dungeons", {
-				templateUrl: "MinecraftDungeons.html"
+				templateUrl: "MinecraftDungeons.html",
+				controller: "MyCtrl",
+				resolve: { $resolve: function() { return "Home" }}
 			})
 			.when("/DLC/:index", {
-				templateUrl: "MinecraftDLC.html"
+				templateUrl: "MinecraftDLC.html",
+				controller: "MyCtrl",
+				resolve: { $resolve: function() { return "Home" }}
 			})
 			.when("/Legends", {
-				templateUrl: "MinecraftLegends.html"
+				templateUrl: "MinecraftLegends.html",
+				controller: "MyCtrl",
+				resolve: { $resolve: function() { return "Home" }}
 			})
 			.when("/Cart", {
-				templateUrl: "MinecraftCart.html"
+				templateUrl: "MinecraftCart.html",
+				controller: "MyCtrl",
+				resolve: { $resolve: function() { return "Cart" }}
 			})
 			.otherwise({
 				redirectTo: "/Dungeons"
